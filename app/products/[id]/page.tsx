417030ea07837cd89dbc75f55e179adaee11613a
unknown <shehzad>
2025-11-09 18:37:07 +0500

    update Asin and dynamic

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -58,7 +58,7 @@ export default async function ProductDetailPage({ params }: ProductDetailPagePro
               </span>
             </div>
 
-            <h1 className="text-[2rem] font-bold text-gray-900 mb-6">
+            <h1 className="text-[1rem] font-bold text-gray-900 mb-6">
               {product.title}
             </h1>
 
